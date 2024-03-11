a33806269156974e3c87474ea9b8d280696caac2
sledoama <160146573+sledoama@users.noreply.github.com>
2024-03-11 11:09:05 -0300

    Synchronizing job pedidos-transformation-bronze-silver from AWS Glue at 2024/03/11 14:09:05

--- pedidos-transformation-bronze-silver/pedidos-transformation-bronze-silver.py
+++ pedidos-transformation-bronze-silver/pedidos-transformation-bronze-silver.py
@@ -1,17 +1,19 @@
 import sys
+import logging
+from datetime import datetime
 from awsglue.transforms import *
 from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
+from pyspark.sql import SparkSession
 from awsglue.context import GlueContext
 from awsglue.job import Job
-from pyspark.sql.types import *
-from awsglue.transforms import *
-from awsglue.utils import getResolvedOptions
-from pyspark.context import SparkContext
-from awsglue.context import GlueContext
 from awsglue.dynamicframe import DynamicFrame
 from pyspark.sql import *
-from pyspark.sql.functions import when, col, lower, regexp_replace, count
+from uuid import uuid4
+from pyspark.sql.types import *
+from pyspark.sql.functions import (
+    col, lower, regexp_replace, year, month, datediff, sum, to_date, countDistinct, avg, trim, current_timestamp, input_file_name, lit
+)
 
 args = getResolvedOptions(sys.argv, ['JOB_NAME'])
 
@@ -22,36 +24,84 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
-# Script generated from Amazon S3
-AmazonS3 = spark.read.options(delimiter=";", header=True).csv("s3://pedidos-bronze/loja")
+# # Sobrescreve as partições necessárias
+# spark.conf.set('spark.sql.sources.partitionOverwriteMode', 'dynamic')
+# spark.conf.set('hive.exec.dynamic.partition.mode', 'nonstrict')
 
-# Tratamento dos tipos de dados
-AmazonS3 = AmazonS3.withColumn("id_cliente", col("id_cliente").cast(StringType())) \
-       .withColumn("data_pedido", col("data_pedido").cast(TimestampType())) \
-       .withColumn("ped_id", col("ped_id").cast(StringType())) \
-       .withColumn("id_produto", col("id_produto").cast(StringType())) \
-       .withColumn("qtd", col("qtd").cast(IntegerType())) \
-       .withColumn("id_canal", col("id_canal").cast(StringType())) \
-       .withColumn("forma_de_pagamento", col("forma_de_pagamento").cast(StringType())) \
-       .withColumn("data_entrega", col("data_entrega").cast(TimestampType())) \
-       .withColumn("nota_fiscal", col("nota_fiscal").cast(StringType()))
+schema = StructType([
+    StructField("id_cliente", StringType(), True),
+    StructField("data_pedido", TimestampType(), True),
+    StructField("ped_id", StringType(), True),
+    StructField("id_produto", StringType(), True),
+    StructField("qtd", IntegerType(), True),
+    StructField("id_canal", StringType(), True),
+    StructField("forma_de_pagamento", StringType(), True),
+    StructField("data_entrega", TimestampType(), True),
+    StructField("nota_fiscal", StringType(), True)
+])
 
-AmazonS3.limit(5).show()
+# Lê o arquivo CSV com o schema definido
+df_silver = spark.read.options(delimiter=";", header=True).schema(schema).csv("s3://pedidos-bronze/staging")
 
-#Tratamento da coluna "forma_de_pagamento" 
-AmazonS3 = AmazonS3.withColumn("forma_de_pagamento", lower(AmazonS3["forma_de_pagamento"])) \
-             .withColumn("forma_de_pagamento", regexp_replace("forma_de_pagamento", "xeque", "cheque")) \
-             .withColumn("forma_de_pagamento", regexp_replace("forma_de_pagamento", "cr[eéèëẽ]+dito", "crédito")) \
-             .withColumn("forma_de_pagamento", regexp_replace("forma_de_pagamento", "d[eéèëẽ]+bito", "débito")) \
-             .withColumn("forma_de_pagamento", regexp_replace("forma_de_pagamento", "pics", "pix")) \
-             .withColumn("forma_de_pagamento", regexp_replace("forma_de_pagamento", "déebito", "débito")) \
-             .withColumn("forma_de_pagamento", regexp_replace("forma_de_pagamento", "crẽditodèbito", "créditodébito"))
+# Script generated from df_silver
+#df_silver = spark.read.options(delimiter=";", header=True).csv("s3://pedidos-bronze/staging")
+#df_silver = spark.read.schema(schema).parquet("s3://pedidos-bronze/bronze/")
 
+# Renomeia colunas e também trata a coluna 'forma_de_pagamento'
+df_silver = (df_silver
+             .withColumnRenamed("ped_id", "id_pedido")
+             .withColumnRenamed("qtd", "quantidade_produto")
+             .withColumn("forma_de_pagamento", lower(col("forma_de_pagamento")))
+             .withColumn("forma_de_pagamento", regexp_replace("forma_de_pagamento", "xeque", "cheque"))
+             .withColumn("forma_de_pagamento", regexp_replace("forma_de_pagamento", "cr[eéèëẽ]+dito", "crédito"))
+             .withColumn("forma_de_pagamento", regexp_replace("forma_de_pagamento", "d[eéèëẽ]+bito", "débito"))
+             .withColumn("forma_de_pagamento", regexp_replace("forma_de_pagamento", "pics", "pix"))
+             .withColumn("forma_de_pagamento", regexp_replace("forma_de_pagamento", "déebito", "débito"))
+             .withColumn("forma_de_pagamento", regexp_replace("forma_de_pagamento", "crẽditodèbito", "crédito-débito"))
+             .withColumn("ano", year("data_pedido"))
+             .withColumn("mes", month("data_pedido"))
+             .withColumn("data_carga", lit(datetime.now().date()))
+             .withColumn("batch_id", lit(str(uuid4())))
+             .withColumn("input_file_name", input_file_name())
+             .withColumn("data_source", lit("pedidos-bronze"))
+             .withColumn("periodo_entrega", datediff(col("data_entrega"), col("data_pedido")))
+            )
 
-# Envie o arquivo para o S3
-#output_path = "s3://pedidos-silver/pedidos_loja/"
-#AmazonS3.write.mode("overwrite").parquet(output_path)
+df_silver = df_silver.select(
+    "id_cliente",
+    "data_pedido",
+    "ano",
+    "mes",
+    "id_pedido",
+    "id_produto",
+    "quantidade_produto",
+    "id_canal",
+    "forma_de_pagamento",
+    "data_entrega",
+    "periodo_entrega",
+    "nota_fiscal",
+    "data_carga",
+    "batch_id",
+    "input_file_name",
+    "data_source"
+)
+
+# s3_bucket_path = "s3://pedidos-silver/pedidos_loja/"
+# mode = "overwrite"
+
+# # Salva o DataFrame em formato Parquet no S3
+# df_silver.write.mode(mode).parquet(s3_bucket_path)
+
+# # Envie o arquivo para o S3
 database = "pedidos-silver"
 tabela = "pedidos_loja"
-output_path = "s3://pedidos-silver/loja/"
-AmazonS3.write.insertInto(f"`{database}`.`{tabela}`", overwrite=True)
+# output_path = "s3://pedidos-silver/pedidos_loja/"
+# df_silver.write.insertInto(f"`{database}`.`{tabela}`", overwrite=True)
+
+# Variáveis
+s3_bucket_path = "s3://pedidos-silver/pedidos_loja/"
+mode = "overwrite"
+database_table_name = f"`{database}`.`{tabela}`"
+
+# Salva o DataFrame como uma nova tabela no catálogo de dados do Glue, especificando o caminho no S3
+df_silver.write.mode(mode).option("path", s3_bucket_path).saveAsTable(database_table_name)
